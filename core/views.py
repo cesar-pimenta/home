934e70911f2eb0ea781e0832794e24efe743682d
Cesar Augusto Pimenta <cesar.pimenta@alstratech.com>
2023-02-16 12:44:42 +0000

    comment_sistem

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 # Create your views here.
 from django.core.mail import send_mail
 
@@ -19,14 +19,45 @@ def post_list(request):
 
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post,
-                             slug=post,
-                             status='published',
-                             publish__year=year,
-                             publish__month=month,
-                             publish__day=day)
-    
-    return render(request, 'core/post/detail.html', {'post': post})
+    post = get_object_or_404(Post, slug=post,
+                                   status='published',
+                                   publish__year=year,
+                                   publish__month=month,
+                                   publish__day=day)
+
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+
+    new_comment = None
+
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    # List of similar posts
+    # post_tags_ids = post.tags.values_list('id', flat=True)
+    # similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+    #                               .exclude(id=post.id)
+    # similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+    #                             .order_by('-same_tags','-publish')[:4]
+
+    return render(request,
+                  'core/post/detail.html',
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,
+                   'comment_form': comment_form,
+                #    'similar_posts': similar_posts
+                })
 
 
 def post_share(request, post_id):
--- landing_page/views.py
+++ landing_page/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def landing_page(requests):
+def landing_page(request):
     return render(request, 'landing_page/landing_page.html', )
