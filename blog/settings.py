243436bcecb6c508a9b78920c13f9f3611b939c3
cesar-pimenta <cesar.pimenta@alstratech.com>
2023-03-01 22:20:03 +0000

    fix

--- blog/settings.py
+++ blog/settings.py
@@ -143,10 +143,10 @@ LOGIN_REDIRECT_URL = 'account:dashboard'
 LOGIN_URL = 'account:login'
 LOGOUT_URL = 'account:logout'
 
-# E-mail
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_HOST = config('EMAIL_HOST', default='')
+# # E-mail
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-EMAIL_PORT = config('EMAIL_PORT', default=25, cast=int)
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
